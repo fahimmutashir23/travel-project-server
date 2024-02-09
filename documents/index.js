101ab00dccd34c8ba3277523f7d5d31cc1edcfd7
fahimmutashir23 <taslimit23@gmail.com>
2024-02-09 07:55:12 +0600

    added invoice template

--- documents/index.js
+++ documents/index.js
@@ -1,15 +1,145 @@
-module.exports = ({name}) => {
-    const today = new Date()
-    return `
-    <!doctype html>
-    <html?>
-    <head>
-    <meta charset="utf-8">
-    <title> Invoice </title>
-    </head>
-    <body>
-    <h1> ${name} </h1>
-    </body>
-    </html?>
-    `
-}
+
+
+module.exports = ( invoice ) => {
+ 
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Invoice</title>
+      <style>
+          body {
+              font-family: Arial, sans-serif;
+          }
+          .invoice {
+              max-width: 800px;
+              margin: 0 auto;
+              padding: 20px;
+              border: 1px solid #ccc;
+              position: relative;
+          }
+          .invoice h2 {
+              text-align: center;
+              margin-top: 0;
+              text-transform: uppercase;
+          }
+          .company-name {
+              text-align: right;
+              font-size: 32px;
+              font-weight: bold;
+              color: green;
+          }
+          .office-address {
+              text-align: right;
+              font-size: 14px;
+              margin-top: 8px;
+          }
+          .flex {
+              position: relative;
+              top: 20px;
+          }
+          .contact-info {
+              text-align: left;
+              margin-top: 50px;
+              font-size: 14px;
+          }
+          .contact-info p {
+              margin-top: -10px;
+          }
+          .invoice-info {
+              text-align: left;
+              position: absolute;
+              left: 0;
+              top: 0;
+          }
+          .invoice-info p {
+              margin: 5px 0;
+          }
+          .invoice table {
+              width: 100%;
+              border-collapse: collapse;
+              margin-top: 20px;
+          }
+          .invoice table th, .invoice table td {
+              padding: 10px;
+              border: 1px solid #ccc;
+          }
+          .invoice table th {
+              background-color: #f2f2f2;
+          }
+          .invoice table td {
+              text-align: center;
+          }
+          .total {
+              margin-top: 20px;
+              text-align: right;
+          }
+          .bottom p {
+              text-align: center;
+              margin-top: 20px;
+              font-weight: 600;
+          }
+          .image {
+              position: absolute;
+              width: 60px;
+              top: 20px;
+              right: 20px;
+          }
+      </style>
+  </head>
+  <body>
+      <div class="invoice">
+          <h2>Invoice</h2>
+          <img src="https://i.postimg.cc/JhLmcHNV/Logo3.png" class="image" alt="">
+         <div class="flex">
+          <div class="invoice-info">
+              <p><strong>Invoice Number:</strong> INV-001</p>
+              <p><strong>Date:</strong> ${invoice?.date}</p>
+          </div>
+          <div>
+              <div class="company-name">
+                  Our Travels
+              </div>
+              <div class="office-address">
+                  Dhanmondi-27, Dhaka-1205 <br>
+                  Bangladesh <br>
+                  Phone: +880 1581 868984 
+              </div>
+          </div>
+         </div>
+          <div class="contact-info">
+              <p><strong>Contact Information:</strong></p>
+              <p>Name: ${invoice?.name} </p>
+              <p>Email: ${invoice?.email}</p>
+              <p>Phone: ${invoice?.phone}</p>
+          </div>
+          <table>
+              <thead>
+                  <tr>
+                      <th>SL</th>
+                      <th>Description</th>
+                      <th>Amount</th>
+                  </tr>
+              </thead>
+              <tbody>
+                  <tr>
+                      <td>1</td>
+                      <td>${invoice?.descriptions}</td>
+                      <td>${invoice?.amount}$</td>
+                  </tr>
+              </tbody>
+          </table>
+          <div class="total">
+              <p><strong>Total:</strong>${invoice?.amount}</p>
+          </div>
+          <div class="bottom">
+              <p>If you have any questions about this invoice, please contact us via email. <br> Must include your Name, Email, and Phone number.</p>
+          </div>
+      </div>
+  </body>
+  </html>
+  
+    `;
+};
--- index.js
+++ index.js
@@ -5,28 +5,23 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const stripe = require("stripe")(process.env.PAYMENT_KEY);
 const nodemailer = require("nodemailer");
-// const pdf = require("html-pdf");
-// const pdfTemplate = require('./documents')
-// const axios = require('axios');
+const pdf = require("html-pdf");
+const pdfTemplate = require("./documents");
+const fs = require("fs");
 
-const port = process.env.PORT || 5000;
-
-
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 app.use(cors());
 
-
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: 465,
-  secure: true,
   auth: {
     user: process.env.SMTP_MAIL,
     pass: process.env.SMTP_PASS,
   },
 });
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.pyhg6t2.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -78,29 +73,28 @@ async function run() {
           name: info.name,
           email: info.email,
           profileImage: info.profileImage,
-          country: info.country
-        }
-      }
-      const result = await userCollection.updateOne(filter, updatedDoc)
-      res.send(result)
-    })
+          country: info.country,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
     app.patch("/users/:id", async (req, res) => {
-      const isAdmin = req.body.response
+      const isAdmin = req.body.response;
       const id = req.params.id;
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
         $set: {
-          admin : isAdmin
-        }
-      }
-      const result = await userCollection.updateOne(filter, updatedDoc)
-      res.send(result)
-    })
+          admin: isAdmin,
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
+    // Hotels APIs -------------------------------------------------------------------------------------
 
-
-    // Hotels APIs
     app.get("/hotels", async (req, res) => {
       const search = req.query.search;
       // const findReserveHotel = await bookingCollection.findOne({
@@ -124,7 +118,7 @@ async function run() {
           ],
         };
       }
-      const result = await hotelCollection.find(query).toArray();
+      const result = await hotelCollection.find(query).limit(8).sort({totalBook : -1}).toArray();
       res.send(result);
     });
 
@@ -149,33 +143,67 @@ async function run() {
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
         $push: {
-          hotel_room: info
-        }
-      }
+          hotel_room: info,
+        },
+      };
       const result = await hotelCollection.updateOne(filter, updatedDoc);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
     app.delete("/hotels/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await hotelCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
+    // Send email Function
+    const sendEmailWithAttachment = async (pdfBuffer, mailInfo) => {
+      const emailFormat = await transporter.sendMail({
+        from: "<mdfahim.muntashir28@gmail.com>",
+        to: mailInfo.to,
+        subject: mailInfo.subject,
+        text: mailInfo.message,
+        attachments: [
+          {
+            filename: "Bookings.pdf",
+            content: pdfBuffer,
+            encoding: "base64",
+          },
+        ],
+      });
+
+      transporter.sendMail(emailFormat, (error, info) => {
+        if (error) {
+          res.send(error);
+        }
+      });
+    };
 
     //PDF Generator
 
-    app.post('/generatePdf', async(req, res) => {
-      pdf.create(pdfTemplate("Fahim"), {}).toFile('result.pdf', (err, buffer) => {
-       if(err){
-         return res.status(500).send({message: 'Generate failed'});
-       }
-       res.setHeader('Content-Type', 'application/pdf')
-       res.send(buffer)
-      });
-     });
+    app.post("/generatePdf", async (req, res) => {
+      const mailInfo = req.body;
+      const invoice = mailInfo.invoiceInfo;
 
+      const fileName = new Date().getTime() + ".pdf";
+      
+      pdf.create(pdfTemplate(invoice), {}).toBuffer((err, buffer) => {
+        if (err) {
+          return res.status(500).send({ message: "Generate failed" });
+        }
+
+        // res.send({ pdfBuffer: buffer })
+        sendEmailWithAttachment(buffer, mailInfo)
+          .then(() => {
+            // console.log("Email sent successfully");
+            res.status(200).send({ message: "Email send successfully" });
+          })
+          .catch((error) => {
+            console.error("Error sending email:", error);
+          });
+      });
+    });
 
     // Bookings Post API
     app.post("/bookings", async (req, res) => {
@@ -183,53 +211,31 @@ async function run() {
       const messageInfo = req.body.emailInfo;
       const result = await bookingCollection.insertOne(info);
       res.send(result);
+    });
 
-      // const invoice = await axios.get('http://localhost:5000/generatePdf', {responseType: 'arraybuffer'});
-      // const invoiceContent = Buffer.from(invoice.data, 'binary').toString('base64')
-
-
-      // const emailFormat = {
-      //   from: '<mdfahim.muntashir28@gmail.com>',
-      //   to: info.email,
-      //   subject: messageInfo.subject,
-      //   text: messageInfo.message,
-      //   attachments: [
-      //     {
-      //       filename: 'Invoice.pdf',
-      //       content: invoiceContent,
-      //       encoding: 'base64'
-      //     }
-      //   ]
-      // };
-
-      const emailFormat = await transporter.sendMail({
-        from: '<mdfahim.muntashir28@gmail.com>',
-        to: info.email,
-        subject: messageInfo.subject,
-        text: messageInfo.message,
-      });
-
-      transporter.sendMail(emailFormat, (error, info) => {
-        if (error) {
-          res.send(error);
-        }
-      })
-      
+    app.patch("/hotel/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $inc: { totalBook: 1 },
+      };
+      const result = await hotelCollection.updateOne(filter, updatedDoc);
+      res.send(result);
     });
 
     app.get("/bookings", async (req, res) => {
       const email = req.query.email;
       const search = req.query.search;
       const category = req.query.category;
-      let filter
+      let filter;
       if (email) {
-        filter = { email: email }
+        filter = { email: email };
       }
-      if(search){
-        filter = { email: { $regex: search, $options: "i" } }
+      if (search) {
+        filter = { email: { $regex: search, $options: "i" } };
       }
-      if(category){
-        filter = {category : category}
+      if (category) {
+        filter = { category: category };
       }
       const result = await bookingCollection.find(filter).toArray();
       res.send(result);
@@ -240,17 +246,17 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await bookingCollection.deleteOne(query);
       res.send(result);
-    })
+    });
 
     app.patch("/bookings/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id : new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
-        $set : {status : 'Done'}
-      }
+        $set: { status: "Done" },
+      };
       const result = await bookingCollection.updateOne(filter, updatedDoc);
       res.send(result);
-    })
+    });
 
     // Payment Intent
     app.post("/payment-intent", async (req, res) => {
@@ -269,76 +275,75 @@ async function run() {
       const data = req.body;
       const result = await packageCollection.insertOne(data);
       res.send(result);
-    })
+    });
 
     app.get("/packages", async (req, res) => {
       const search = req.query.search;
       const limit = parseInt(req.query.limit);
-      let query
-      if(search){
+      let query;
+      if (search) {
         query = {
-          $or : [
-            { name : { $regex: search, $options: "i" } },
-            { destination : { $regex: search, $options: "i" } }
-          ]
-        }
+          $or: [
+            { name: { $regex: search, $options: "i" } },
+            { destination: { $regex: search, $options: "i" } },
+          ],
+        };
       }
       const result = await packageCollection.find(query).limit(limit).toArray();
       res.send(result);
-    })
+    });
 
     app.get("/packages/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id : new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) };
       const result = await packageCollection.findOne(filter);
       res.send(result);
-    })
+    });
 
-    app.delete('/packages/:id', async (req, res) => {
+    app.delete("/packages/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await packageCollection.deleteOne(query);
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-    app.patch('/packages/:id', async (req, res) => {
+    app.patch("/packages/:id", async (req, res) => {
       const id = req.params.id;
       const discount = req.body;
       const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
         $set: {
-          discount: discount.discountRate
-        }
-      }
+          discount: discount.discountRate,
+        },
+      };
       const result = await packageCollection.updateOne(filter, updatedDoc);
-      res.send(result)
-    })
+      res.send(result);
+    });
     app.get("/stats", async (req, res) => {
       try {
-         const users= await userCollection.estimatedDocumentCount()
-         const totalPackages=  await packageCollection.estimatedDocumentCount()
-         const totalBookings=  await bookingCollection.estimatedDocumentCount()
-         const result=  await bookingCollection.aggregate([
-          {
-            $group:{
-              _id:null,
-              totalRevenue:{
-                $sum:'$roomPrice'
-              }
-            }
-          }
-         ]).toArray()
-         revenue= result.length> 0 ? result[0].totalRevenue: 0;
-         res.send({users, totalPackages, totalBookings, revenue})
-      }
-      catch (err) {
+        const users = await userCollection.estimatedDocumentCount();
+        const totalPackages = await packageCollection.estimatedDocumentCount();
+        const totalBookings = await bookingCollection.estimatedDocumentCount();
+        const result = await bookingCollection
+          .aggregate([
+            {
+              $group: {
+                _id: null,
+                totalRevenue: {
+                  $sum: "$roomPrice",
+                },
+              },
+            },
+          ])
+          .toArray();
+        revenue = result.length > 0 ? result[0].totalRevenue : 0;
+        res.send({ users, totalPackages, totalBookings, revenue });
+      } catch (err) {
         console.log(err);
       }
-    })
-
+    });
 
     //---------------------------------------------------------------------------------------//
-
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
